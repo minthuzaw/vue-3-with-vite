693a07cd05df86a68dbdda2c2977e685691a2077
Min Thu Zaw <minthuzaw775@gmail.com>
2023-03-13 08:39:04 +0630

    added: script set up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+
+// eslint-disable-next-line no-undef
+let message = $ref("Hello World!");
+// let message = ref("Hello World!");
+// setTimeout(() => {
+//   message.value = "I have been changed!";
+// }, 2000);
+
+setTimeout(() => {
+  message = "I have been changed!";
+}, 2000);
+
+let alertSomething = () => {
+  alert("Do Something!")
+};
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
+    <input type="text" v-model="message" />
+
+    <button @click="alertSomething">Click</button>
   </main>
 </template>
